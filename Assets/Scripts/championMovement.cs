6a6e5bd4ca13d5816b2fb8a0b6920109d8b31429
JackShort <john.short.tj@gmail.com>
2016-02-06 18:14:20 -0500

    movemnt

---
+++ Assets/Scripts/championMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class championMovement : MonoBehaviour {
+
+	public float moveSpeed = 1f;
+	private Vector3 mousePosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1)) {
+			mousePosition = Input.mousePosition;
+			mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+		}
+
+		if((transform.position.x != mousePosition.x) && (transform.position.y != mousePosition.y)) {
+			transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), new Vector2(mousePosition.x, mousePosition.y), moveSpeed * Time.deltaTime);
+		}
+	}
+}
