76e60ff624cd255e23e8b804415e6f0f74fc31d8
JackShort <john.short.tj@gmail.com>
2016-02-06 20:00:07 -0500

    alfkdj

--- Assets/Scripts/championMovement.cs
+++ Assets/Scripts/championMovement.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class championMovement : MonoBehaviour {
 
 	public float moveSpeed = 1f;
+	public float range = 5f;
 	private Vector3 mousePosition;
+	private bool needsToMove = false;
+	private Transform transformToMoveTo;
 
 	// Use this for initialization
 	void Start () {
@@ -16,9 +19,31 @@ public class championMovement : MonoBehaviour {
 		if (Input.GetMouseButton(1)) {
 			mousePosition = Input.mousePosition;
 			mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+			RaycastHit2D hit = Physics2D.Raycast(new Vector2(mousePosition.x, mousePosition.y), new Vector2(0, 0));
+
+			if(hit.collider != null) {
+				float distance = Vector2.Distance(transform.position, hit.transform.position);
+				Debug.Log(Vector2.Distance(transform.position, hit.transform.position));
+				if(distance > range) {
+					needsToMove = true;
+					transformToMoveTo = hit.transform;
+					mousePosition = hit.transform.position;
+				} else {
+					mousePosition = transform.position;
+				}
+			}
 		}
 
 		if((transform.position.x != mousePosition.x) && (transform.position.y != mousePosition.y)) {
+			if(needsToMove == true) {
+				Debug.Log(Vector2.Distance(transform.position, transformToMoveTo.position));
+				if(Vector2.Distance(transform.position, transformToMoveTo.position) <= 5) {
+					mousePosition = transform.position;
+					needsToMove = false;
+				}
+			}
+
 			transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), new Vector2(mousePosition.x, mousePosition.y), moveSpeed * Time.deltaTime);
 		}
 	}
