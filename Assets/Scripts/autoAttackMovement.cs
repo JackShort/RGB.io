d53e447bf31ee56abb3f7df79d8400bb0975f53f
JackShort <john.short.tj@gmail.com>
2016-02-06 21:33:52 -0500

    adfldj

---
+++ Assets/Scripts/autoAttackMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class autoAttackMovement : MonoBehaviour {
+	public Transform target;
+	public float movementSpeed = 9f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void damageTarget() {
+		Debug.Log("BOOM!!!!");
+		Destroy(this.gameObject);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), new Vector2(target.position.x, target.position.y), movementSpeed * Time.deltaTime);
+		if(transform.position == target.position) {
+			damageTarget();
+		}
+	}
+}
--- Assets/Scripts/championMovement.cs
+++ Assets/Scripts/championMovement.cs
@@ -5,13 +5,23 @@ public class championMovement : MonoBehaviour {
 
 	public float moveSpeed = 1f;
 	public float range = 5f;
+	public float autoAttackCooldown = 1.5f;
+	public GameObject projectile;
+
 	private Vector3 mousePosition;
 	private bool needsToMove = false;
 	private Transform transformToMoveTo;
+	private float autoAttackTimer;
+	private bool shouldAutoAttack = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		autoAttackTimer = autoAttackCooldown;
+	}
+
+	void autoAttack() {
+		projectile.GetComponent<autoAttackMovement>().target = transformToMoveTo;
+		Instantiate(projectile, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
@@ -20,31 +30,50 @@ public class championMovement : MonoBehaviour {
 			mousePosition = Input.mousePosition;
 			mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
+			//for clicking on enemies
 			RaycastHit2D hit = Physics2D.Raycast(new Vector2(mousePosition.x, mousePosition.y), new Vector2(0, 0));
 
-			if(hit.collider != null) {
+			if((hit.collider != null) && (hit.collider.name != "player")) {
 				float distance = Vector2.Distance(transform.position, hit.transform.position);
-				Debug.Log(Vector2.Distance(transform.position, hit.transform.position));
 				if(distance > range) {
 					needsToMove = true;
 					transformToMoveTo = hit.transform;
+					Debug.Log(transformToMoveTo);
 					mousePosition = hit.transform.position;
+					shouldAutoAttack = true;
 				} else {
+					transformToMoveTo = hit.transform;
 					mousePosition = transform.position;
+					shouldAutoAttack = true;
 				}
+			} else {
+				shouldAutoAttack = false;
 			}
 		}
 
 		if((transform.position.x != mousePosition.x) && (transform.position.y != mousePosition.y)) {
-			if(needsToMove == true) {
-				Debug.Log(Vector2.Distance(transform.position, transformToMoveTo.position));
+			if(needsToMove == true) {       //moves to auto attack range
 				if(Vector2.Distance(transform.position, transformToMoveTo.position) <= 5) {
 					mousePosition = transform.position;
 					needsToMove = false;
 				}
 			}
-
+					//moves to mouse position
 			transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), new Vector2(mousePosition.x, mousePosition.y), moveSpeed * Time.deltaTime);
+		}
+
+		//auto attack nonsense
+		if(shouldAutoAttack == true) {
+			if(autoAttackTimer == autoAttackCooldown) {
+				autoAttack();
+				autoAttackTimer -= Time.deltaTime;
+			}
+		}
+
+		if ((autoAttackTimer > 0) && (autoAttackTimer != autoAttackCooldown)) {
+				autoAttackTimer -= Time.deltaTime;
+		} else {
+				autoAttackTimer = autoAttackCooldown;
 		}
 	}
 }
