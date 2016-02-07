a7e6fa66bd490d7dc16e06030c78ac670933a687
TJSlava <tjhsstslava@gmail.com>
2016-02-06 23:22:08 -0500

    Health bar and health numbers

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,20 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
 	public float maxHealth = 100.0f;	
 	public float currentHealth = 0.0f;
+	public Text health; 
 	public GameObject HealthBar;
 	// Use this for initialization
 	void Start () {
 		currentHealth = maxHealth;
-		InvokeRepeating ("decreaseHealth", 1f, 1f);
+		health.text = "" + currentHealth;
+		//health = GetComponent<Text> ();
+		//health.text = "FUck you";
+		//InvokeRepeating ("decreaseHealth", 1f, 1f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//health = GetComponent<Text> ();
+		//health = GameObject.GetComponent<Text>();
+		//health.text = "Hi" + currentHealth;
 	}
 
 	void decreaseHealth(){
